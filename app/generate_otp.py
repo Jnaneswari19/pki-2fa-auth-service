10ee52bc14ab68fcb2470be9805f74e926d1a43b
Jnaneswari <jnaneswaridasam@gmail.com>
2025-12-19 10:03:45 +0530

    Reset repo to rebuild PKI 2FA microservice

--- app/generate_otp.py
+++
@@ -1,5 +0,0 @@
-import pyotp
-
-def generate_totp(secret: str) -> str:
-    totp = pyotp.TOTP(secret)
-    return totp.now()
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start cron in background
-service cron start
-
-# Start FastAPI with uvicorn
-uvicorn app.main:app --host 0.0.0.0 --port 8000
